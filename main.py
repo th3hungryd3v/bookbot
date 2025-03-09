f29dfa0a0943c9278c47bdf7b72e3f6c2d6b4df9
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-09 09:15:53 -0700

    sys.arg is imported and being used to check if a path (second argument) is provided

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import word_count
 from stats import character_count
 from stats import sort_counts
@@ -8,12 +9,17 @@ def get_book_text(filepath):
     return book_content
 
 def main():
-    filepath = "books/frankenstein.txt"
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     count_words = word_count(book_text)
     char_count = character_count(book_text)
     sortedcounts = sort_counts(char_count)
-    
+
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {filepath}...")
     print("----------- Word Count ----------")
