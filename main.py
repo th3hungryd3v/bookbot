262bc201a6b05dc3b2dbe0b7359323695dbf3fa3
th3hungryd3v <th3hungryd3v@gmail.com>
2025-03-08 17:57:38 -0800

    C: add character count function, print to console

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import word_count
+from stats import character_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         book_content = f.read()
@@ -7,8 +10,10 @@ def main():
     filepath = "books/frankenstein.txt"
     book_text = get_book_text(filepath)
     count_words = word_count(book_text)
-    # print(book_text)
+    char_count = character_count(book_text)
     print(f"{count_words} words found in the document")
+    print(f"Character Count:")
+    print(f"{char_count}")
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+def character_count(text):
+    store = {} # empty dict
+    text = text.lower() # convert all chars to lowercase
+
+    for char in text: # iterate each char
+        if char in store: # check if the character is already in the dict
+            store[char] += 1 # add 1 if it exists
+        else:
+            store[char] = 1 # add char to dict with a count of 1
+    return store
